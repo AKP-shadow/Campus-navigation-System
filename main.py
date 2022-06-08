83d9ecd72d2bb2fe26fd9906aa6f1ad3ac28e781
Aadhithya K Praveen <78066049+AKP-shadow@users.noreply.github.com>
2022-06-08 20:40:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -99,9 +99,9 @@ def start(src,dest):
     mark_source_dest(src_pt,dest_pt,m)
     draw_route(src_pt,dest_pt,m)
     
-    return m.get_root().render()
+#     return m.get_root().render()
 #     print('sdas')
-#     m.save("./templates/__map.html")
+    m.save("./templates/__map.html")
 
 def reset():
     m = folium.Map(location = [12.752812133105126, 80.19658495347934],zoom_start = 20,zoom_control=False,)
